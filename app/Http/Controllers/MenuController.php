6bcfbd15c63799d02b7b281b6277ead8493d618d
lsatriosim <liefran123@gmail.com>
2022-06-01 20:31:54 +0700

    Validasi dan Alert Session tambah Menu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Input;
 use App\Models\Mpasi;
 use DateTime;
+use Dotenv\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -32,19 +33,31 @@ class MenuController extends Controller
     }
 
     public function storemenu(Request $request){
-        $today = new DateTime();
-        $input = new Input();
-        $input->user_id = Auth::id();
-        $input->status = 0;
-        $input->nama_menu = $request->namaMenu;
-        $input->deskripsi_menu = $request->deskripsi;
-        $input->umur_bayi = $request->umurBayi;
-        $input->bahan_menu = $request->bahan;
-        $input->langkah_memasak = $request->langkah;
-        $input->created_at = $today;
-        $input->updated_at = $today;
-        $input->save();
+        $this->validate($request, [
+            'namaMenu' => 'required',
+            'deskripsi' => 'required|max:255',
+            'umurBayi' => 'required|numeric',
+            'bahan' => 'required|max:255',
+            'langkah' => 'required|max:255',
+        ]);
 
-        return redirect()->back()->with('status', 'Menu Added Successfully');
+        if(Mpasi::where('nama', $request->namaMenu)->count() !== 0){
+            $today = new DateTime();
+            $input = new Input();
+            $input->user_id = Auth::id();
+            $input->status = 0;
+            $input->nama_menu = $request->namaMenu;
+            $input->deskripsi_menu = $request->deskripsi;
+            $input->umur_bayi = $request->umurBayi;
+            $input->bahan_menu = $request->bahan;
+            $input->langkah_memasak = $request->langkah;
+            $input->created_at = $today;
+            $input->updated_at = $today;
+            $input->save();
+
+            return redirect()->back()->with('success', 'Menu Added Successfully');
+        }else{
+            return redirect()->back()->with('fail', 'Fail to Add Menu');
+        };
     }
 }
--- resources/views/tambahmenu.blade.php
+++ resources/views/tambahmenu.blade.php
@@ -1,7 +1,19 @@
 @extends('template')
 @section('container')
-@if (session('status'))
-            <h6 class="alert alert-success">{{ session('status') }}</h6>
+@if (session('success'))
+            <h6 class="alert alert-success">{{ session('success') }}</h6>
+@endif
+@if (session('fail'))
+            <h6 class="alert alert-danger">{{ session('fail') }}</h6>
+@endif
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+             @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+             @endforeach
+        </ul>
+    </div>
 @endif
 
 <div class="container mb-2">
@@ -10,31 +22,31 @@
         <div class="col-12 m-2">
             <div class="row">
                 <label for="namaMenu" class="col-3">Nama Menu</label>
-                <input type="text" class="form-control" id="namaMenu" placeholder="Masukkan nama menu..." class="col-9" name="namaMenu">
+                <input type="text" class="form-control" id="namaMenu" placeholder="Masukkan nama menu..." class="col-9" name="namaMenu" value="{{old('namaMenu')}}" required>
             </div>
         </div>
         <div class="col-12 m-2">
             <div class="row">
                 <label for="deskripsi" class="col-3">Deskripsi</label>
-                <textarea class="form-control" id="deskripsi" class="col-9" name="deskripsi">Masukkan deskripsi menu...</textarea>
+                <textarea class="form-control" id="deskripsi" class="col-9" name="deskripsi" required value="{{old('deskripsi')}}"></textarea>
             </div>
         </div>
         <div class="col-12 m-2">
             <div class="row">
                 <label for="umurBayi" class="col-3">Untuk Bayi dengan Umur</label>
-                <input type="number" class="form-control" id="umurBayi" placeholder="Masukkan umur bayi..." class="col-9" name="umurBayi">
+                <input type="number" class="form-control" id="umurBayi" placeholder="Masukkan umur bayi..." class="col-9" name="umurBayi" required value="{{old('umurBayi')}}">
             </div>
         </div>
         <div class="col-12 m-2">
             <div class="row">
                 <label for="bahan" class="col-3">Bahan Menu</label>
-                <textarea class="form-control" id="bahan" class="col-9" name="bahan">Masukkan bahan menu...</textarea>
+                <textarea class="form-control" id="bahan" class="col-9" name="bahan" required value="{{old('bahan')}}"></textarea>
             </div>
         </div>
         <div class="col-12 m-2">
             <div class="row">
                 <label for="langkah" class="col-3">Langkah Memasak</label>
-                <textarea class="form-control" id="langkah" class="col-9" name="langkah">Masukkan langkah memasak menu...</textarea>
+                <textarea class="form-control" id="langkah" class="col-9" name="langkah" required value="{{old('langkah')}}"></textarea>
             </div>
         </div>
         <button type="submit" class="btn btn-primary rounded-2">Tambah</button>
